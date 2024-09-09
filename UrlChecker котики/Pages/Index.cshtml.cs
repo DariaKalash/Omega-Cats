9269ea8680d264a5d74ce623369efcbb2c4b7916
DariaKalash <kalasnikova.dg@gmail.com>
2024-09-09 13:49:32 +0300

    lalala

---
+++ UrlChecker котики/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HTTP_Cat.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string CatUrl { get; set; }
+
+        public async Task OnPostAsync(string url)
+        {
+            // Отправить GET-запрос на указанный URL
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+
+                // Получить код состояния ответа
+                var statusCode = (int)response.StatusCode;
+
+                // Получить изображение кота с сервиса https://http.cat/
+                CatUrl = $"https://http.cat/{statusCode}";
+            }
+        }
+    }
+}
